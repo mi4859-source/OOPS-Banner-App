6486818a7d465c65dff8c822b7102d2fc27b63d5
mi4859-source <mi4859@srmist.edu.in>
2026-02-15 14:54:33 +0530

    UC2: Fixed ASCII alignment for OOPS banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,15 @@
-/**
- * @author Ismaeel
- * @version 1.0
- * Implementation of Use Case 1: Printing "OOPS" to the console.
- */
-public class OOPSBannerApp {
-    public static void main(String[] args) {
-        // This prints the literal text OOPS to the console
-        System.out.println("OOPS");
-    }
+public class OOPSBannerApp
+{
+public static void main(String args[])
+{
+System.out.println("      ******      "   +   "       ******      "   +   "  ********      "   +   "     ******      " );
+System.out.println("    **      **    "   +   "     **      **    "   +   "  **      **    "   +   "   **            " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **        **  "   +   "  **             " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **      **    "   +   "    **           " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  ** ******     "   +   "      ****       " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "           **    " );
+System.out.println("  **          **  "   +   "   **          **  "   +   "  **            "   +   "            **   " );
+System.out.println("    **      **    "   +   "     **      **    "   +   "  **            "   +   "          **     " );
+System.out.println("      ******      "   +   "       ******      "   +   "  **            "   +   "    ******       " );
+}
 }
