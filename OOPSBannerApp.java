299cb365e757b6b58d55d7b1edee1c3bea9cc546
mi4859-source <mi4859@srmist.edu.in>
2026-02-23 07:58:32 +0530

    Refactor OOPSBannerApp class structure

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,7 +1,5 @@
-public class OOPSBannerApp
-{
-public static void main(String args[])
-{
+public class OOPSBannerApp{
+public static void main(String args[]){
         System.out.println(String.join(" ", "  ***  ", "  ***  ", " ****  ", " ***** "));
         System.out.println(String.join(" ", " *   * ", " *   * ", " *   * ", " *     "));
         System.out.println(String.join(" ", "*     *", "*     *", " ****  ", " ****  "));
